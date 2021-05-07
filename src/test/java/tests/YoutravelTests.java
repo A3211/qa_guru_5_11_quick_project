b5e9fff5490fb881322b245d6f1188a5abc1adc7
A3211 <a.kusl1122@gmail.com>
2021-05-07 23:57:32 +0700

    added tests

---
+++ src/test/java/tests/YoutravelTests.java
@@ -0,0 +1,85 @@
+package tests;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.SelenideElement;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+import static io.qameta.allure.Allure.step;
+
+public class YoutravelTests {
+
+    /*
+    https://youtravel.me/
+
+    + 1. Страница открылась появилась надпись
+    + 2. Сменили язык, проверили что надпись изменилась
+    3. Авторизация через гугл
+    + 4. Поиск тура куда, когда, найти
+     */
+
+    @Test
+    @DisplayName("Successful open youtravel.me")
+    void openMainPageTest() {
+        step("Open https://youtravel.me/", () -> {
+            open("https://youtravel.me/");
+            $("body").shouldHave(Condition.text("АВТОРСКИЕ ТУРЫ — НОВЫЙ ФОРМАТ НАСЫЩЕННЫХ ПУТЕШЕСТВИЙ"));
+        });
+    }
+
+    @Test
+    @DisplayName("Successful change language to english")
+    void changeLanguageTest() {
+        step("Open https://youtravel.me/", () -> {
+            open("https://youtravel.me/");
+            $("body").shouldHave(Condition.text("АВТОРСКИЕ ТУРЫ — НОВЫЙ ФОРМАТ НАСЫЩЕННЫХ ПУТЕШЕСТВИЙ"));
+        });
+
+        step("Change language to english", () -> {
+            SelenideElement languageMenu = $(byText("Язык"));
+            actions().moveToElement(languageMenu).perform();
+            $(byText("Английский (English)")).click();
+            $("body").shouldHave(Condition.text("GROUP TOURING AS PERSONAL AS NEVER BEFORE"));
+        });
+    }
+
+    @Test
+    @DisplayName("Successful find a tour")
+    void findTourTest() {
+        Configuration.startMaximized = true;
+
+        step("Open https://youtravel.me/", () -> {
+            open("https://youtravel.me/");
+            $("body").shouldHave(Condition.text("АВТОРСКИЕ ТУРЫ — НОВЫЙ ФОРМАТ НАСЫЩЕННЫХ ПУТЕШЕСТВИЙ"));
+        });
+
+        step("Choosing Altai", () -> {
+            $(byText("Куда")).scrollIntoView(true).click();
+            $(byText("Алтай")).scrollIntoView(true).click();
+        });
+
+        step("Choosing date", () -> {
+            $(byText("Когда")).scrollIntoView(true).click();
+            $(byText("Сен")).scrollIntoView(true).click();
+        });
+
+        step("Click find", () -> {
+            $(".filter-main__send-btn").click();
+        });
+
+        step("Check Successful open page", () -> {
+            $("body").shouldHave(Condition.text("Туры на Алтай"));
+        });
+    }
+
+    
+
+
+
+
+
+}
